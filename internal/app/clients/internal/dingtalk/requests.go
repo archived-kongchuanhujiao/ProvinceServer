66337c01003f0d1f02cda6571eb5a3b5b724facb
千橘雫霞 <qianjunakasumi@gmail.com>
2021-01-02 17:18:33 +0800

    缺陷：修复请求未发送

--- internal/app/clients/internal/dingtalk/requests.go
+++ internal/app/clients/internal/dingtalk/requests.go
@@ -24,7 +24,8 @@ func SendDTMessage(c *DingTalk, m *clientspublic.Message) bool {
 		return false
 	}
 
-	_, ok := request(c, sendMsg, json)
+	errs, ok := request(c, sendMsg, json)
+	loggerr.Info("发送消息", zap.Any("你看看", errs))
 
 	return ok
 }
@@ -32,16 +33,15 @@ func SendDTMessage(c *DingTalk, m *clientspublic.Message) bool {
 func request(c *DingTalk, subUrl string, json []byte) (errRes ErrResponse, ok bool) {
 	ts, sign := generateSign(c.Secret)
 
-	req, err := http.NewRequest(http.MethodPost, mainAPI+subUrl+c.AccessToken+"&timestamp="+ts+"&sign="+sign, bytes.NewReader(json))
-
+	resp, err := http.Post(
+		mainAPI+subUrl+c.AccessToken+"&timestamp="+ts+"&sign="+sign,
+		"application/json",
+		bytes.NewReader(json),
+	)
 	if err != nil {
 		loggerr.Warn("调用钉钉接口出现异常", zap.Error(err))
 		return
 	}
-
-	req.Header.Add("Content-Type", "application/json")
-
-	resp := req.Response
 
 	if resp.StatusCode < 200 || resp.StatusCode > 300 {
 		loggerr.Warn("调用钉钉接口出现异常, 响应码: " + strconv.Itoa(resp.StatusCode))
@@ -49,14 +49,12 @@ func request(c *DingTalk, subUrl string, json []byte) (errRes ErrResponse, ok bo
 	}
 
 	b, err := ioutil.ReadAll(resp.Body)
-
 	if err != nil {
 		loggerr.Warn("解析钉钉接口传回状态异常", zap.Error(err))
 		return
 	}
 
 	err = json2.Unmarshal(b, &errRes)
-
 	if err != nil {
 		loggerr.Warn("解析钉钉接口传回状态异常", zap.Error(err))
 		return
