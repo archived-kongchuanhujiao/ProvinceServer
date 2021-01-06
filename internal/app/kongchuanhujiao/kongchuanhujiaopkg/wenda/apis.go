1c0d5a8fad33abdc52a5e1d2e3d600654264723d
千橘雫霞 <qianjunakasumi@gmail.com>
2021-01-07 00:06:10 +0800

    任务：API MVC 标准化和问题更新数据 API

--- internal/app/kongchuanhujiao/kongchuanhujiaopkg/wenda/apis.go
+++ internal/app/kongchuanhujiao/kongchuanhujiaopkg/wenda/apis.go
@@ -5,68 +5,87 @@ import (
 	"coding.net/kongchuanhujiao/server/internal/app/client/clientmsg"
 	"coding.net/kongchuanhujiao/server/internal/app/datahub/datahubpkg"
 	"coding.net/kongchuanhujiao/server/internal/app/datahub/datahubpkg/wenda"
-	"github.com/kataras/iris/v12/mvc"
+	"coding.net/kongchuanhujiao/server/internal/pkg/logger"
+
+	"go.uber.org/zap"
 )
 
 type (
 	APIs struct{} // APIs 问答 APIs
 
-	GetQuestionReq struct { // GetQuestionReq 问题请求数据
-		Page uint32 `json:"page"` // 页面
-		ID   uint32 `json:"id"`   // 唯一识别码
+	Response struct { // Response 响应
+		Status  uint16      `json:"status"`  // 状态码
+		Message string      `json:"message"` // 消息
+		Data    interface{} `json:"data"`    // 数据
+	}
+
+	GetQuestionsReq struct { // GetQuestionsReq 问题请求数据
+		Page uint32 // 页面
+		ID   uint32 // 唯一识别码
+	}
+
+	PutQuestionReq struct { // PutQuestionReq 问题更新数据
+		ID     uint32 // 唯一识别码
+		Status uint8  // 状态
 	}
 
 	GetMarketsReq struct { // GetMarketsReq 市场请求数据
-		Page    uint32 `json:"page"` // 页面
-		Subject uint8  `json:"sub"`  // 学科
+		Page    uint32 // 页面
+		Subject uint8  // 学科
 	}
 
 	POSTPraisePeq struct { // POSTPraisePeq 表扬请求数据
-		ID   uint32   `json:"id"`   // 唯一识别码
-		List []uint64 `json:"list"` // 名单
+		ID   uint32   // 唯一识别码
+		List []uint64 // 名单
 	}
 
 	POSTPUSHCENTERReq struct {
-		ID     uint32 `json:"id"`     // 唯一识别码
-		Target string `json:"target"` // 目标
+		ID     uint32 // 唯一识别码
+		Target string // 目标
 	}
 )
 
-func (a *APIs) BeforeActivation(_ mvc.BeforeActivation) {}
-
-// 获取状态。
-// GET /apis/wenda/
-func (a *APIs) Get() map[string]string { return map[string]string{"status": "online"} }
-
-// 获取问题列表或问题。
+// GetQuestions 获取问题列表或问题。
 // GET /apis/wenda/questions
-func (a *APIs) GetQuestions(v *GetQuestionReq) interface{} {
+func (a *APIs) GetQuestions(v *GetQuestionsReq) *Response {
 	if v.ID != 0 {
-		return wenda.GetQuestions(0, v.ID, false, 0)[0]
+		return &Response{0, "ok", wenda.GetQuestions(0, v.ID, false, 0)[0]}
 	}
-	return wenda.GetQuestions(v.Page, v.ID, false, 0)
+	return &Response{0, "ok", wenda.GetQuestions(v.Page, v.ID, false, 0)}
 }
 
-// 推送表扬列表。
+// PutQuestions 更新问题状态。
+// PUT /apis/wenda/questions
+func (a *APIs) PutQuestions(v *PutQuestionReq) *Response {
+	err := wenda.UpdateQuestions(v.ID, v.Status)
+	if err != nil {
+		logger.Error("错误", zap.Error(err))
+		return &Response{Status: 1, Message: "服务器错误"}
+	}
+	return &Response{Status: 0, Message: "ok"}
+}
+
+// PostPraise 推送表扬列表。
 // POST /apis/wenda/praise
-func (a *APIs) PostPraise(v *POSTPraisePeq) {
+func (a *APIs) PostPraise(v *POSTPraisePeq) *Response {
 	q := wenda.GetQuestions(0, v.ID, false, 0)
 	msg := clientmsg.NewTextMessage("表扬下列答对的同学：\n")
 	for _, mem := range v.List {
 		msg.AddAt(mem)
 	}
 	client.GetClient().SendMessage(msg.SetTarget(&clientmsg.Target{Group: &clientmsg.Group{ID: q[0].Target}}))
+	return &Response{Status: 0, Message: "ok"}
 }
 
-// 获取市场列表。
+// GetMarkets 获取市场列表。
 // GET /apis/wenda/markets
-func (a *APIs) GetMarkets(v *GetMarketsReq) []wenda.QuestionListTab {
-	return wenda.GetQuestions(v.Page, 0, true, v.Subject)
+func (a *APIs) GetMarkets(v *GetMarketsReq) *Response {
+	return &Response{0, "ok", wenda.GetQuestions(v.Page, 0, true, v.Subject)}
 }
 
-// 推送数据到钉钉。
+// PostPUSHCENTER 推送数据到钉钉。
 // POST /apis/wenda/pushcenter
-func (a *APIs) PostPUSHCENTER(v *POSTPUSHCENTERReq) {
+func (a *APIs) PostPUSHCENTER(v *POSTPUSHCENTERReq) *Response {
 
 	/*
 		TODO 通过ID 读取问题。
@@ -85,5 +104,5 @@ func (a *APIs) PostPUSHCENTER(v *POSTPUSHCENTERReq) {
 
 		datahubpkg.PushMessage("fakeToken", "fakeSecret", "", []string{}, false)
 	}
-
+	return &Response{0, "", nil}
 }
