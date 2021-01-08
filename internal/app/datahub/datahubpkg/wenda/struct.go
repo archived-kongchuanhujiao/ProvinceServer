e6db620ebcc1e1f601c47a0820e525454473d39d
千橘雫霞 <qianjunakasumi@gmail.com>
2021-01-08 13:03:45 +0800

    重构：字段名修改简化

--- internal/app/datahub/datahubpkg/wenda/struct.go
+++ internal/app/datahub/datahubpkg/wenda/struct.go
@@ -2,15 +2,15 @@ package wenda
 
 type (
 	QuestionsTab struct { // QuestionsTab 问题
-		ID        uint32 `json:"id" db:"id"`                 // 唯一标识符
-		Type      uint8  `json:"type" db:"type"`             // 类型
-		Subject   uint8  `json:"subject" db:"subject"`       // 学科
-		Question  string `json:"question" db:"question"`     // 问题
-		CreatorID string `json:"creator_id" db:"creator_id"` // 创建者
-		Target    uint64 `json:"target" db:"target"`         // 目标
-		Status    uint8  `json:"status" db:"status"`         // 状态
-		Options   string `json:"options" db:"options"`       // 选项
-		Key       string `json:"key" db:"key"`               // 答案
-		Market    bool   `json:"market" db:"market"`         // 存在市场
+		ID       uint32 `json:"id" db:"id"`             // 唯一标识符
+		Type     uint8  `json:"type" db:"type"`         // 类型
+		Subject  uint8  `json:"subject" db:"subject"`   // 学科
+		Question string `json:"question" db:"question"` // 问题
+		Creator  string `json:"creator" db:"creator"`   // 创建者
+		Target   uint64 `json:"target" db:"target"`     // 目标
+		Status   uint8  `json:"status" db:"status"`     // 状态
+		Options  string `json:"options" db:"options"`   // 选项
+		Key      string `json:"key" db:"key"`           // 答案
+		Market   bool   `json:"market" db:"market"`     // 存在市场
 	}
 )
