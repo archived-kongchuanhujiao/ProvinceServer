8530b1bb316bf7bf85482ca8e5a5f678aaed370d
千橘雫霞 <qianjunakasumi@gmail.com>
2021-01-10 14:59:36 +0800

    史诗：结构体和结构包

--- internal/app/datahub/datahubpkg/wenda/struct.go
+++ internal/app/datahub/datahubpkg/wenda/struct.go
@@ -2,6 +2,7 @@ package wenda
 
 import (
 	"coding.net/kongchuanhujiao/server/internal/pkg/logger"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -20,6 +21,19 @@ type (
 		Options  string `json:"options" db:"options"`   // 选项
 		Key      string `json:"key" db:"key"`           // 答案
 		Market   bool   `json:"market" db:"market"`     // 存在市场
+	}
+
+	AnswersTab struct { // AnswersTab 回答
+		ID       uint32    `json:"id" db:"id"`             // 唯一标识符
+		Question uint32    `json:"question" db:"question"` // 问题
+		QQ       uint64    `json:"qq" db:"qq"`             // QQ
+		Answer   string    `json:"answer" db:"answer"`     // 回答
+		Time     time.Time `json:"time" db:"time"`         // 时刻
+	}
+
+	CalculationsTab struct { // CalculationsTab 计算结果
+		Question uint32 `json:"question" db:"question"` // 问题
+		Data     string `json:"data" db:"data"`         // 数据 TODO string 更改为具体的结构体
 	}
 
 	runtime map[uint32][]*websocket.Conn // runtime 运行时
---
+++ internal/app/kongchuanhujiaopublic/wendapkg/struct.go
@@ -0,0 +1,5 @@
+package wendapkg
+
+// TODO 所有底层需要用到的结构、类型
+
+type ()
