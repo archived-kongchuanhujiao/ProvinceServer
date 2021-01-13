9f75acfc9d3e850f07ed25c9520970d4395751a1
千橘雫霞 <qianjunakasumi@gmail.com>
2021-01-13 22:28:24 +0800

    重构：取消注册功能

--- internal/app/kongchuanhujiao/accounts/apis.go
+++ internal/app/kongchuanhujiao/accounts/apis.go
@@ -17,18 +17,6 @@ import (
 	"go.uber.org/zap"
 )
 
-// TODO https://qianjunakasumi.coding.net/p/kongchuanhujiao/requirements/issues/15/detail
-/*
-TODO
-   MVC API 参考 wenda 包接入
-   注册账号 API 实现步骤：
-   接收POST注册账号 POST /apis/accounts/register/verifier 要求QQ字段 QQ：uint64
-   向QQ号发送好友消息（消息内容：验证码,4位，有效期5min）
-   客户端 POST /apis/accounts/register  要求 QQ 、验证码和ID 字段  验证码：uint8 id:string
-   验证 验证码：通过map[uint64(QQ)]uint8(验证码) // 对比，如果一致则注册成功
-   向数据库写入，数据API我后面会写
-*/
-
 type (
 	APIs struct{} // APIs 账号 APIs
 
@@ -52,14 +40,6 @@ func (a *APIs) PostCode(v *PostCodeReq) *kongchuanhujiao.Response {
 	return &kongchuanhujiao.Response{Message: "ok"}
 }
 
-// 获取验证码并发送
-// POST apis/accounts/register/verifier
-func (a *APIs) PostRegisterVerifier(c *context.Context) *kongchuanhujiao.Response {
-	id := c.PostValue("QQ")
-	a.PostCode(&PostCodeReq{ID: id})
-	return &kongchuanhujiao.Response{Message: "ok"}
-}
-
 // 登录。
 // POST apis/accounts/login
 func (a *APIs) PostLogin(v *PostLoginReq, c *context.Context) *kongchuanhujiao.Response {
@@ -74,10 +54,6 @@ func (a *APIs) PostLogin(v *PostLoginReq, c *context.Context) *kongchuanhujiao.R
 	// TODO 生成Token
 
 	return &kongchuanhujiao.Response{Message: "ok"}
-
-}
-
-func (a *APIs) GetLogin() {
 
 }
 
