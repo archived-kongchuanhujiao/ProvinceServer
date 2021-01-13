8fde5c1dae9538f16cb06879339294ebd47b0d86
千橘雫霞 <qianjunakasumi@gmail.com>
2021-01-13 22:19:06 +0800

    史诗：登录校验

--- internal/app/kongchuanhujiao/accounts/apis.go
+++ internal/app/kongchuanhujiao/accounts/apis.go
@@ -2,8 +2,8 @@ package accounts
 
 import (
 	"errors"
-	"github.com/kataras/iris/v12/context"
 	"math/rand"
+	"net/http"
 	"strconv"
 	"time"
 
@@ -11,6 +11,10 @@ import (
 	"coding.net/kongchuanhujiao/server/internal/app/client/clientmsg"
 	"coding.net/kongchuanhujiao/server/internal/app/datahub/datahubpkg/accounts"
 	"coding.net/kongchuanhujiao/server/internal/app/kongchuanhujiao"
+	"coding.net/kongchuanhujiao/server/internal/pkg/logger"
+
+	"github.com/kataras/iris/v12/context"
+	"go.uber.org/zap"
 )
 
 // TODO https://qianjunakasumi.coding.net/p/kongchuanhujiao/requirements/issues/15/detail
@@ -29,16 +33,21 @@ type (
 	APIs struct{} // APIs 账号 APIs
 
 	PostCodeReq struct{ ID string } // PostCodeReq 验证码发送
+
+	PostLoginReq struct { // PostLoginReq 登录验证
+		ID   string // 唯一识别码
+		Code string // 验证码
+	}
 )
 
-var code map[string]string = map[string]string{} // code 验证码
+var code = map[string]string{} // code 验证码
 
 // 验证码。
 // POST apis/accounts/code
 func (a *APIs) PostCode(v *PostCodeReq) *kongchuanhujiao.Response {
 	err := sendCode(v.ID)
 	if err != nil {
-		return &kongchuanhujiao.Response{Status: 1, Message: "服务器错误"}
+		return &kongchuanhujiao.Response{Status: 1, Message: err.Error()}
 	}
 	return &kongchuanhujiao.Response{Message: "ok"}
 }
@@ -53,7 +62,18 @@ func (a *APIs) PostRegisterVerifier(c *context.Context) *kongchuanhujiao.Respons
 
 // 登录。
 // POST apis/accounts/login
-func (a *APIs) PostLogin() {
+func (a *APIs) PostLogin(v *PostLoginReq, c *context.Context) *kongchuanhujiao.Response {
+	if v.Code != code[v.ID] || v.Code == "" {
+		return &kongchuanhujiao.Response{Status: 1, Message: "验证码有误"}
+	}
+
+	c.SetCookie(&http.Cookie{
+		Name: "account", Value: v.ID, Path: "/", Expires: time.Now().AddDate(0, 1, 0),
+	})
+
+	// TODO 生成Token
+
+	return &kongchuanhujiao.Response{Message: "ok"}
 
 }
 
@@ -62,10 +82,11 @@ func (a *APIs) GetLogin() {
 }
 
 // sendCode 发送验证码
-func sendCode(id string) error {
+func sendCode(id string) (err error) {
 	a, err := accounts.SelectAccount(id, 0)
 	if err != nil {
-		return err
+		logger.Error("发送验证码错误", zap.Error(err))
+		return errors.New("服务器错误")
 	}
 
 	if len(a) == 0 {
@@ -78,5 +99,5 @@ func sendCode(id string) error {
 	m := clientmsg.NewTextMessage("您的验证码是： " + c + " 。任何人都不会索要验证码！")
 	client.GetClient().SendMessage(m.SetTarget(&clientmsg.Target{ID: a[0].QQ}))
 	code[id] = c
-	return nil
+	return
 }
