e3c8bc4232ab59a3e95611f4beeb2ded684aeb77
千橘雫霞 <qianjunakasumi@gmail.com>
2021-01-14 13:15:54 +0800

    修复：标签写错了

--- internal/app/kongchuanhujiao/public/wendapkg/struct.go
+++ internal/app/kongchuanhujiao/public/wendapkg/struct.go
@@ -2,8 +2,8 @@ package wendapkg
 
 type (
 	QuestionField []struct { // QuestionField 问题字段
-		Type string `json:"type"`    // 类型
-		Data string `json:"content"` // 数据
+		Type string `json:"type"` // 类型
+		Data string `json:"data"` // 数据
 	}
 
 	OptionsField []string // OptionsField 选项字段
