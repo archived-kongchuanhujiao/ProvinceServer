08d79ce81277c317f570ebb59e70d1a3b458f7ac
千橘雫霞 <qianjunakasumi@gmail.com>
2021-01-15 23:57:13 +0800

    史诗：缓存功能 API 示范

---
+++ internal/app/datahub/datahubpkg/wenda/runtime_cache.go
@@ -0,0 +1,15 @@
+package wenda
+
+import "coding.net/kongchuanhujiao/server/internal/app/kongchuanhujiao/public/wendapkg"
+
+var caches = map[uint32]*wendapkg.WendaDetails{}
+
+// AddCache 新增缓存
+func AddCache() {
+
+}
+
+// RemoveCache 移除缓存
+func RemoveCache() {
+
+}
