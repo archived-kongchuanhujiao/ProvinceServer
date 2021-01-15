53d7fd42fd9879ca33e1cf9c2eb8f7e5ad32136d
千橘雫霞 <qianjunakasumi@gmail.com>
2021-01-16 00:41:23 +0800

    史诗：新增问题(问答) ID 的定义

--- internal/app/kongchuanhujiao/public/wendapkg/struct.go
+++ internal/app/kongchuanhujiao/public/wendapkg/struct.go
@@ -3,13 +3,15 @@ package wendapkg
 import "coding.net/kongchuanhujiao/server/internal/app/datahub/datahubpkg/wenda"
 
 type (
+	QuestionID uint32 // 问题（问答） ID
+
 	// WendaDetails 问答详情
 	// 内部缓存结构体
 	// 不应该被返回给 API 客户端
 	WendaDetails struct {
 		// 包含问题和回答还有群人的数据
 		Questions *wenda.QuestionsTab // 问题
-		Answers   *wenda.AnswersTab   // 回答
+		Answers   []*wenda.AnswersTab // 回答
 		Members   *GroupMembers       // 群成员
 	}
 
