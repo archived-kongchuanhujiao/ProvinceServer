a9aaef421bd1e7eafb36a73e19a37aab6335afc4
千橘雫霞 <qianjunakasumi@gmail.com>
2021-01-16 00:52:44 +0800

    重构：公开变量以便外部查询

--- internal/app/datahub/datahubpkg/wenda/runtime_cache.go
+++ internal/app/datahub/datahubpkg/wenda/runtime_cache.go
@@ -3,6 +3,6 @@ package wenda
 import "coding.net/kongchuanhujiao/server/internal/app/kongchuanhujiao/public/wendapkg"
 
 var (
-	caches      = map[uint32]*wendapkg.WendaDetails{} // caches 缓存
-	activeGroup = map[uint64]wendapkg.QuestionID{}    // activeGroup 活动的群
+	Caches      = map[wendapkg.QuestionID]*wendapkg.WendaDetails{} // Caches 缓存
+	ActiveGroup = map[uint64]wendapkg.QuestionID{}                 // ActiveGroup 活动的群
 )
