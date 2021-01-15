63aae14535b5fcba5f29db18c3b3250bfe180227
千橘雫霞 <qianjunakasumi@gmail.com>
2021-01-15 23:40:42 +0800

    史诗：数据库新增回答函数

---
+++ internal/app/datahub/datahubpkg/wenda/answers.go
@@ -0,0 +1,22 @@
+package wenda
+
+import (
+	"coding.net/kongchuanhujiao/server/internal/app/datahub/internal/maria"
+	"github.com/elgris/sqrl"
+	"go.uber.org/zap"
+)
+
+// InsertAnswer 新增回答
+func InsertAnswer(a *AnswersTab) (err error) {
+	sql, args, err := sqrl.Insert("answers").Values(nil, a.Question, a.QQ, a.Answer, a.Time).ToSql()
+	if err != nil {
+		loggerr.Error("生成SQL语句失败", zap.Error(err))
+		return
+	}
+
+	_, err = maria.DB.Exec(sql, args...)
+	if err != nil {
+		maria.Logger.Error("插入失败", zap.Error(err), zap.String("SQL语句", sql))
+	}
+	return
+}
--- internal/app/datahub/datahubpkg/wenda/questions.go
+++ internal/app/datahub/datahubpkg/wenda/questions.go
@@ -53,7 +53,7 @@ func UpdateQuestionStatus(id uint32, status uint8) (err error) {
 	return
 }
 
-// InsertQuestion 新建问题
+// InsertQuestion 新增问题
 func InsertQuestion(q *QuestionsTab) (err error) {
 	sql, args, err := sqrl.Insert("questions").Values(nil, q.Type, q.Subject, q.Question, q.Creator,
 		q.Target, 0, q.Options, q.Key, q.Market).ToSql()
