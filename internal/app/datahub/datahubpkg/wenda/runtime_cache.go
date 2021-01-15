caacf394a4f3f29f84ede0993694975dc70340b5
千橘雫霞 <qianjunakasumi@gmail.com>
2021-01-16 00:40:56 +0800

    史诗：新增活动群与活动问题 ID 的映射

--- internal/app/datahub/datahubpkg/wenda/runtime_cache.go
+++ internal/app/datahub/datahubpkg/wenda/runtime_cache.go
@@ -2,14 +2,7 @@ package wenda
 
 import "coding.net/kongchuanhujiao/server/internal/app/kongchuanhujiao/public/wendapkg"
 
-var caches = map[uint32]*wendapkg.WendaDetails{}
-
-// AddCache 新增缓存
-func AddCache() {
-
-}
-
-// RemoveCache 移除缓存
-func RemoveCache() {
-
-}
+var (
+	caches      = map[uint32]*wendapkg.WendaDetails{} // caches 缓存
+	activeGroup = map[uint64]wendapkg.QuestionID{}    // activeGroup 活动的群
+)
