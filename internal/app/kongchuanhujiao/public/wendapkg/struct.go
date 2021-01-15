31cbe5b522c7ed1160594ecc232d8fd6def39308
千橘雫霞 <qianjunakasumi@gmail.com>
2021-01-15 23:57:52 +0800

    史诗：公共包新增结构体信息

--- internal/app/kongchuanhujiao/public/wendapkg/struct.go
+++ internal/app/kongchuanhujiao/public/wendapkg/struct.go
@@ -1,6 +1,21 @@
 package wendapkg
 
+import "coding.net/kongchuanhujiao/server/internal/app/datahub/datahubpkg/wenda"
+
 type (
+	// WendaDetails 问答详情
+	// 内部缓存结构体
+	// 不应该被返回给 API 客户端
+	WendaDetails struct {
+		// 包含问题和回答还有群人的数据
+		Questions *wenda.QuestionsTab // 问题
+		Answers   *wenda.AnswersTab   // 回答
+		Members   *GroupMembers       // 群成员
+	}
+
+	Groups       map[uint64]string // Groups 群
+	GroupMembers map[uint64]string // GroupMembers 群成员
+
 	QuestionField []struct { // QuestionField 问题字段
 		Type string `json:"type"` // 类型
 		Data string `json:"data"` // 数据
