16a71f34015abe742e2dd96c1156dd35aea293e2
StarWishsama <starwishsama@outlook.com>
2021-01-16 17:01:07 +0800

    史诗：问答数据在服务端计算 #17

---
+++ internal/app/datahub/datahubpkg/wenda/calculate.go
@@ -0,0 +1,44 @@
+package wenda
+
+import "coding.net/kongchuanhujiao/server/internal/app/kongchuanhujiao/public/wendapkg"
+
+func calculateQuestion(w *wendapkg.WendaDetails) (calc *wendapkg.CalculationsTab) {
+	var (
+		rightStus []uint64
+		wrongStus [][]uint64
+	)
+
+	rightStus = []uint64{}
+	wrongStus = [][]uint64{}
+
+	correctAnswer := w.Questions.Key
+
+	for _, ans := range w.Answers {
+		if ans.Answer == correctAnswer {
+			rightStus = append(rightStus, ans.QQ)
+		} else {
+			for i, option := range w.Questions.Options {
+				if option != ans.Answer {
+					continue
+				}
+
+				op := wrongStus[i]
+				if op == nil {
+					op = []uint64{}
+				}
+
+				op = append(op, ans.QQ)
+				wrongStus[i] = op
+			}
+		}
+	}
+
+	calc = &wendapkg.CalculationsTab{
+		Question:    uint32(w.Questions.ID),
+		AnswerCount: uint8(len(w.Answers)),
+		Right:       rightStus,
+		Wrong:       wrongStus,
+	}
+
+	return
+}
