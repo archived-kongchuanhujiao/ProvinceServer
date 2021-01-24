e997e262fe9ba4505d8557be755f58cfb72d0719
千橘 雫霞 <qianjunakasumi@gmail.com>
2021-01-24 19:23:03 +0800

    重构：账号表删除 Type 字段

--- internal/app/datahub/datahubpkg/accounts/struct.go
+++ internal/app/datahub/datahubpkg/accounts/struct.go
@@ -6,7 +6,6 @@ var loggerr = logger.Named("数据总线").Named("账号")
 
 type Tab struct { // Tab 账号
 	ID    string `json:"id" db:"id"`       // 唯一标识符
-	Type  uint8  `json:"type" db:"type"`   // 类型
 	QQ    uint64 `json:"qq" db:"qq"`       // QQ
 	Class uint32 `json:"class" db:"class"` // 班级
 	Push  string `json:"push" db:"push"`   // 推送
