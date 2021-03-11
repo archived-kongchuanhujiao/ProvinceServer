88c442a21ffaa46b77a77e8641ab26b50d9d5ffa
千橘 雫霞 <i@qianjunakasumi.ren>
2021-03-11 13:23:21 +0800

    其他：同一个世界，同一个IntelliJ IDEA，规范化开发流程

---
+++ .idea/build.ps1
@@ -0,0 +1,14 @@
+﻿$module = "github.com/kongchuanhujiao/server"
+$main   = "cmd/server/main.go"
+$commit = git rev-parse --short HEAD
+
+$env:GOOS   = "linux"
+$env:GOARCH = "amd64"
+
+"构建信息：CommitShortID ${commit}"
+
+"开始构建..."
+
+go build -ldflags "-w -s -X main.Commit=${commit}" -o .kongchuanhujiao/serve ${main}
+
+"构建完成"
