610ad5cd536afa20b55d5ecd701687a536c8b243
千橘 雫霞 <i@qianjunakasumi.ren>
2021-03-16 23:21:07 +0800

    重构：帐号表删除 Token 字段，明确 Push 字段类型

--- internal/app/datahub/public/account/struct.go
+++ internal/app/datahub/public/account/struct.go
@@ -1,10 +1,17 @@
 package account
 
-type Tab struct { // Tab 账号
-	ID    string `json:"id" db:"id"`       // 唯一标识符
-	Type  uint8  `json:"type" db:"type"`   // 类型
-	QQ    uint64 `json:"qq" db:"qq"`       // QQ
-	Class uint32 `json:"class" db:"class"` // 班级
-	Push  string `json:"push" db:"push"`   // 推送
-	Token string `json:"token" db:"token"` // Token
-}
+type (
+	// Tab 账号
+	Tab struct {
+		ID    string    `json:"id" db:"id"`       // 标识号
+		QQ    uint64    `json:"qq" db:"qq"`       // QQ
+		Class uint32    `json:"class" db:"class"` // 班级 FIXME 是否需要
+		Push  PushField `json:"push" db:"push"`   // 推送
+	}
+
+	// PushField 推送字段
+	PushField []struct {
+		Platform string `json:"platform"` // 平台
+		Key      string `json:"key"`      // 键
+	}
+)
