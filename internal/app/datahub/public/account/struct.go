a4e29d5faf515fad7dee484c8b8308f75028fee9
千橘 雫霞 <i@qianjunakasumi.ren>
2021-03-16 23:52:54 +0800

    重构：数据库帐号表解析

--- internal/app/datahub/public/account/struct.go
+++ internal/app/datahub/public/account/struct.go
@@ -12,6 +12,7 @@ type (
 	// PushField 推送字段
 	PushField []struct {
 		Platform string `json:"platform"` // 平台
-		Key      string `json:"key"`      // 键
+		Key      string `json:"key"`      // 键[ QQ群号, 钉钉AccessToken ]
+		Secret   string `json:"secret"`   // 机密[ QQ平台留空，钉钉平台为密钥 ]
 	}
 )
