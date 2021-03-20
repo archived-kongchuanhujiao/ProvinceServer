ba665b66ff2f28710afb958862453a1833bb1758
StarWishsama <starwishsama@outlook.com>
2021-03-20 21:56:04 +0800

    修复：未检查消息类型

--- internal/app/kongchuanhujiao/ciyun/handle.go
+++ internal/app/kongchuanhujiao/ciyun/handle.go
@@ -9,14 +9,16 @@ import (
 // HandleWordStat 处理词云
 func HandleWordStat(m *message.Message) {
 
-	ct := m.Chain[0].(*message.Text).Content
+	ct, ok := m.Chain[0].(*message.Text)
 
-	// 不处理空消息
-	if len(ct) == 0 {
-		return
+	if ok {
+		// 不处理空消息
+		if len(ct.Content) == 0 {
+			return
+		}
+
+		words := client.GetClient().ExtractWords(ct.Content)
+
+		ciyun.PushWord(m.Target.Group.ID, words)
 	}
-
-	words := client.GetClient().ExtractWords(ct)
-
-	ciyun.PushWord(m.Target.Group.ID, words)
 }
